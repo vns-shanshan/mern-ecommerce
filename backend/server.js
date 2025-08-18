e637adf7399550d09708f083639696153185f77a
huijingj <vanessajin0426@gmail.com>
2025-08-18 16:07:59 -0700

    0 - Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import { connectDB } from './lib/db.js';
+
 import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
@@ -12,4 +14,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(5000, () => {
     console.log("Server is running on http://localhost:" + PORT);
+    connectDB()
 })
