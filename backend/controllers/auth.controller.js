f8ef375a8ec0ad2f054b99d8a3fe93c99ef8d5be
huijingj <vanessajin0426@gmail.com>
2025-08-21 15:33:24 -0700

    1 - Added logout and signin controllers. Also adding refreshToken controller to validate the user auth again.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -12,7 +12,7 @@ const generateTokens = (userId) => {
 }
 
 const storeRefreshToken = async (userId, refreshToken) => {
-    await redis.set(`refreshToken:${userId}`, refreshToken, "EX", 60 * 60 * 24 * 7); // 7 days
+    await redis.set(`refresh_token:${userId}`, refreshToken, "EX", 60 * 60 * 24 * 7); // 7 days
 }
 
 const setCookies = (res, accessToken, refreshToken) => {
@@ -53,22 +53,96 @@ export const signup = async (req, res) => {
         setCookies(res, accessToken, refreshToken);
 
         res.status(201).json({
-            user: {
-                _id: user._id,
-                email: user.email,
-                name: user.name,
-                role: user.role
-            }, message: "User created successfully"
+            _id: user._id,
+            email: user.email,
+            name: user.name,
+            role: user.role
         });
     } catch (error) {
+        console.log("Error in signup controller:", error.message)
         res.status(500).json({ message: error.message });
     }
 }
 
 export const login = async (req, res) => {
-    res.send("Login route");
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (user && (await user.comparePassword(password))) {
+            const { accessToken, refreshToken } = generateTokens(user._id);
+
+            await storeRefreshToken(user._id, refreshToken);
+
+            setCookies(res, accessToken, refreshToken)
+
+            res.json({
+                _id: user._id,
+                email: user.email,
+                name: user.name,
+                role: user.role,
+            })
+        } else {
+            res.status(401).json({ message: "Invalid email or password" });
+        }
+    } catch (error) {
+        console.log("Error in login controller:", error.message)
+        res.status(500).json({ message: error.message });
+    }
 }
 
 export const logout = async (req, res) => {
-    res.send("Logout route");
+    try {
+        const refreshToken = req.cookies.refreshToken;
+
+        if (refreshToken) {
+            const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+            await redis.del(`refresh_token:${decoded.userId}`)
+
+            res.clearCookie("accessToken");
+            res.clearCookie("refreshToken");
+
+            res.json({ message: "Logged out successfully" });
+        }
+    } catch (error) {
+        console.log("Error in logout controller:", error.message)
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
 }
+
+// This will refresh the access token
+export const refreshToken = async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken
+
+        if (!refreshToken) {
+            return res.status(401).json({ message: "No refresh token provided" });
+        }
+
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+        const storedToken = await redis.get(`refresh_token:${decoded.userId}`);
+
+        if (storedToken !== refreshToken) {
+            return res.status(403).json({ message: "Invalid refresh token" });
+        }
+
+        const accessToken = jwt.sign({ userId: decoded.userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: 15 * 60 * 1000 // 15 minutes
+        });
+
+        res.json({ message: "Token refreshed successfully" })
+    } catch (error) {
+        console.log("Error in refreshToken controller:", error.message);
+        return res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+// TODO: implement getProfile function later
+// export const getProfile = async (req, res) => {
+
+// }
