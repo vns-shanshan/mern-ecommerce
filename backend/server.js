850684400fe475eb8f3a5a24af2e16ef052d4342
huijingj <vanessajin0426@gmail.com>
2025-08-25 16:59:00 -0700

    1 - Added analytics routes.

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import productRoutes from './routes/product.route.js';
 import cartRoutes from './routes/cart.route.js';
 import couponRoutes from './routes/coupon.route.js';
 import paymentRoutes from './routes/payment.route.js';
+import analyticsRoutes from "./routes/analytic.route.js"
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use("/api/products", productRoutes)
 app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes)
 app.use("/api/payments", paymentRoutes)
+app.use("/api/analytics", analyticsRoutes)
 
 app.listen(5000, () => {
     console.log("Server is running on http://localhost:" + PORT);
