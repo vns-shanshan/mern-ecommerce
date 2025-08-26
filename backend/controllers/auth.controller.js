2583156e2e53e73b83ef41b35eee948a0db3ec41
huijingj <vanessajin0426@gmail.com>
2025-08-25 17:02:14 -0700

    1 - Added a getProfile controller in auth routes.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -142,7 +142,11 @@ export const refreshToken = async (req, res) => {
     }
 }
 
-// TODO: implement getProfile function later
-// export const getProfile = async (req, res) => {
-
-// }
+export const getProfile = async (req, res) => {
+    try {
+        res.json(req.user)
+    } catch (error) {
+        console.log("Error in getProfile controller:", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,16 +1,16 @@
 import express from 'express';
-import { login, logout, signup, refreshToken } from '../controllers/auth.controller.js';
+
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { login, logout, signup, refreshToken, getProfile } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/signup", signup)
-
 router.post("/login", login)
-
 router.post("/logout", logout)
 
 router.post("/refresh-token", refreshToken)
 
-// router.get("/profile", protectRoute, getProfile)
+router.get("/profile", protectRoute, getProfile)
 
 export default router;
